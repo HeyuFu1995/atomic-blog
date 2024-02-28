67d173b04d64dec9e0629fc3bd33c188383c7280
Heyu Fu <Herry Fu>
2024-02-28 14:13:01 +1100

    using Memo Family

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { memo, useEffect, useState } from "react";
-import { PostProvider, usePosts } from "./Components/PostContext";
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
+import { usePosts } from "./Components/PostContext";
 import Test from "./Test";
 
 // 1) Create a new Context
 
 function App() {
   const [isFakeDark, setIsFakeDark] = useState(false);
+  const { posts, onAddPost } = usePosts();
+
+  const archiveOption = useMemo(() => {
+    return {
+      show: false,
+      title: `Post archieve in addition to ${posts.length} main posts`,
+    }
+  }, [posts.length]);
+
+  const handleAddPost = useCallback(function (post) {
+    onAddPost(post);
+  }, []);
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
@@ -25,12 +37,12 @@ function App() {
       >
         {isFakeDark ? "☀️" : "🌙"}
       </button>
-      <PostProvider>
-        <Header />
-        <Main />
-        <Archive show={false} />
-        <Footer />
-      </PostProvider >
+
+      <Header />
+      <Main />
+      <Archive archiveOption={archiveOption} onAddPost={handleAddPost} />
+      <Footer />
+
     </section>
 
   );
@@ -134,20 +146,20 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function ({ show }) {
+const Archive = memo(function ({ archiveOption, onAddPost }) {
 
-  const { onAddPost, createRandomPost } = usePosts();
+  const { createRandomPost } = usePosts();
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(show);
+  const [showArchive, setShowArchive] = useState(archiveOption.show);
 
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{archiveOption.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
